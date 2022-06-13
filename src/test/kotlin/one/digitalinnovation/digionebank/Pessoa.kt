4f4e77e0cb99886edb718187826ac25a239f3ca7
FlavioBarcel <barcelos.fsg@gmail.com>
2022-06-13 16:14:34 -0300

    Udate to apply the private mofifier to set function of cpdf property

--- src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,13 +3,12 @@ package one.digitalinnovation.digionebank
 class Pessoa {
    var nome:String="Jether"
    var cpf:String="123.123.123.111"
-   //private set    // Deixou como private somente a função que atribui valor aquela properiedade
+   private set    // Deixou como private somente a função que atribui valor aquela properiedade
 
 }
 
 fun main() {
     val jether=Pessoa()
-    jether.cpf="345"
 
     println(jether)
     println(jether.nome)
