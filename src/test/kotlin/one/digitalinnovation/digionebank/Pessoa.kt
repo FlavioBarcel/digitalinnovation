a2a1bd41dbe0c4ceae8d4c0026fe752493a919bc
FlavioBarcel <barcelos.fsg@gmail.com>
2022-06-13 16:45:25 -0300

    Update to talk about class members

--- src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,14 +5,16 @@ class Pessoa {
    var cpf:String="123.123.123.111"
    private set    // Deixou como private somente a função que atribui valor aquela properiedade
 
+   //constructor()   //costrutor secundario
+   fun personaInfo() = "A pessoa se chama $nome e tem o numero de CPF: $cpf"
+
 }
 
 fun main() {
     val jether=Pessoa()
 
-    println(jether)
-    println(jether.nome)
-    println(jether.cpf)
+    println(jether.personaInfo())
+
 
 
 }
