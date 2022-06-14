ee16f70e9c2b06f2d6b0369a8030e3d5242aff09
FlavioBarcel <barcelos.fsg@gmail.com>
2022-06-14 12:46:33 -0300

    Atualização feitas

--- src/test/kotlin/one/digitalinnovation/digionebank/Conta.kt
+++ src/test/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -2,19 +2,15 @@ package one.digitalinnovation.digionebank
 
 import java.math.BigDecimal
 
-class Conta {
-    val  agencia:String,
+class Conta (
+    val agencia:String,
     val numero:String,
     val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+    fun saque(valor: BigDecimal) {
 
 }
-fun deposito(valor:BigDecimal){
-
-
-
-}
-
-fun saque(valor:BigDecimal){
-
-
 }
--- src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/test/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,7 +5,7 @@ class Pessoa {
    var cpf:String="123.123.123.111"
    private set    // Deixou como private somente a função que atribui valor aquela properiedade
 
-   //constructor()   //costrutor secundario
+   constructor()   //costrutor secundario
    fun personaInfo() = "A pessoa se chama $nome e tem o numero de CPF: $cpf"
 
 }
